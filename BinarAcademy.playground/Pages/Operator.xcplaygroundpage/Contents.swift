4ac980918560df7705485a4decb54a97c4bcdd21
Dimas Purnomo <dimzpurnomo@gmail.com>
2022-02-16 22:01:03 +0700

    Add typedata, operator aritmatika, operator logika

---
+++ BinarAcademy.playground/Pages/Operator.xcplaygroundpage/Contents.swift
@@ -0,0 +1,31 @@
+//: [Previous](@previous)
+
+import Foundation
+import Darwin
+
+//operator aritmatika
+
+var result: Double
+var numberFirst: Double = 3
+var numberTwo: Double = 3
+
+result = numberFirst + numberTwo
+result = numberFirst - numberTwo
+result = numberFirst * numberTwo
+result = numberFirst / numberTwo
+result = pow(numberFirst, 2)
+result = sqrt(result)
+
+var isEven : Bool
+
+isEven = numberFirst == numberTwo
+isEven = numberFirst > numberTwo
+isEven = numberFirst < numberTwo
+isEven = numberFirst >= numberTwo
+isEven = numberFirst <= numberTwo
+
+
+//: [Next](@next)
+
+
+
---
+++ BinarAcademy.playground/Pages/Typedata.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting: String = "Hello, playground"
+var numberFloat: Float = 2
+var numberDouble: Double = 2
+var isTrue: Bool = true
+
+
+print(numberFloat)
+
+//tuple
+var (nama, umur, alamat): (String, Int, String) = ("Dimas Purnomo", 21, "Lembah Griya Indah Blok G2 No.3")
+nama
+umur
+alamat
+
